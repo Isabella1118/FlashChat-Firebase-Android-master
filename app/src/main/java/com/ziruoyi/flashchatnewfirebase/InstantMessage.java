ef03a433083d4e937ac8a7c39536d7fa5b67133f
Isabella1118 <yiziruowh@gmail.com>
2018-01-22 21:10:25 -0600

    Initial commit

---
+++ app/src/main/java/com/ziruoyi/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,26 @@
+package com.ziruoyi.flashchatnewfirebase;
+
+/**
+ * Created by Isabel on 1/20/18.
+ */
+
+public class InstantMessage {
+    private String message;
+    private String author;
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
